faa2fd0ecb48f4a0e8daee27cb0d259c856e0e42
lmcollier <lmariecollier@gmail.com>
2019-05-14 16:58:45 -0500

    Initial Check In

---
+++ C2/AverageNumbers/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        double a = 4.5;
+        double b = 13.4;
+        double c = 12.3;
+        double d = 42.0;
+
+        double average = (a+b+c+d)/4;
+        System.out.println("The average is: " + average);
+    }
+}
