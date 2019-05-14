a7aed98f4b6f813b44373f397245f78abc0c9a46
lmcollier <lmariecollier@gmail.com>
2019-05-14 17:08:10 -0500

    Checking in again

---
+++ C2/Factorial/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int fac4 = 4*2*3;
+        System.out.println(fac4);
+
+        int fac7 = 7*6*5*fac4;
+        System.out.println(fac7);
+    }
+}
