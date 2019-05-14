1322a4759d7e6b4860a64b74afabc3cf22f2f851
lmcollier <lmariecollier@gmail.com>
2019-05-14 17:01:34 -0500

    Initial Check In

---
+++ C2/Total/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int x = 3;
+        int y = 4;
+        int total = x + y;
+        System.out.println("total = " +total);
+    }
+}
