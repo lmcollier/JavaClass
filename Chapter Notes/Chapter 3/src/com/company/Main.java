dff0d92add4453863df05f92f2c30bb943e24225
lmcollier <lmariecollier@gmail.com>
2019-05-14 16:42:42 -0500

    Initial check in

---
+++ Chapter Notes/Chapter 3/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        long x = 10000000000000L; //have to put the letter L after the number to tell Java that it is a long number
+
+        double r = Math.PI; //has 16 digits of precision
+        float b = 1.5f; //has 8 digits of precision.
+        //primarily use these for scientific or more precise calculations
+
+        System.out.println(r);
+    }
+}
