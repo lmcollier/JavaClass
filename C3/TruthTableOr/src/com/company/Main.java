c598405e46c16fd8ae99c528d96ef36e98e72e6e
lmcollier <lmariecollier@gmail.com>
2019-05-15 11:20:52 -0500

    Chap3 Truth Table Or

---
+++ C3/TruthTableOr/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        boolean a = false;
+        boolean b = false;
+        boolean compare = (a||b);
+        System.out.println("     Truth Table");
+        System.out.println("  a      b  |  a || b  ");
+        System.out.println("-----------------------");
+        System.out.println("false false |  " + compare);
+
+        a = false;
+        b = true;
+        compare = (a||b);
+        System.out.println("false  true |  " + compare);
+
+        a = true;
+        b = false;
+        compare = (a||b);
+        System.out.println("true  false |  " + compare);
+
+        a = true;
+        b = true;
+        compare = (a||b);
+        System.out.println("true  true  |  " + compare);
+    }
+}
