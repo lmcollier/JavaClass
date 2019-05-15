27a4d87c4d32c6d55a4665f27a20f3123bc0b510
lmcollier <lmariecollier@gmail.com>
2019-05-15 12:29:37 -0500

    Chap3 Integer Extremes

---
+++ C3/IntExtremes/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int positivInt = 2147483647;
+        System.out.println(positivInt);
+
+        positivInt++;
+        System.out.println(positivInt);
+
+        int negativeInt = -2147483648;
+        System.out.println(negativeInt);
+
+        negativeInt--;
+        System.out.println(negativeInt);
+    }
+}
