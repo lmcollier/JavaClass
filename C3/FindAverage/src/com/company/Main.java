c71d4ca818f1d20c2257663d6b85890d0b925b3d
lmcollier <lmariecollier@gmail.com>
2019-05-15 11:06:22 -0500

    Chap3 Find Average

---
+++ C3/FindAverage/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        double a = 5.0;
+        double b = 6.0;
+        double c = 7.0;
+        double d = 8.0;
+
+        d = 12.0;
+
+        double avg = (a+b+c+d)/4.0;
+
+        System.out.println("The average is: " + avg);
+    }
+}
