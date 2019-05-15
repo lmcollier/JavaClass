f3dfc7610c908000428a55c7c2fbbcd643783969
lmcollier <lmariecollier@gmail.com>
2019-05-15 12:32:02 -0500

    Chap3 Integer by Zero

---
+++ C3/IntByZero/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int a = 42;
+        int b = 0;
+        int result = a/b;
+        System.out.println(result);
+    }
+}
