2a3455ec5afc295fcbcb67e13a4fd210339d2d16
lmcollier <lmariecollier@gmail.com>
2019-05-15 12:33:45 -0500

    Chap3 Double by Zero

---
+++ C3/DoubleByZero/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        double a = 42.0;
+        double b = 0.0;
+        double result = a/b;
+        System.out.println(result);
+    }
+}
