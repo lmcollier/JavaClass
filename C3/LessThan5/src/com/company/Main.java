d36908f7ddc881f4b8c87a35f0229f2cdca89c24
lmcollier <lmariecollier@gmail.com>
2019-05-15 11:28:20 -0500

    Chap3 Less than 5

---
+++ C3/LessThan5/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int x = 0;
+        boolean compare = (x < 5);
+        System.out.println(compare);
+
+        x = 1;
+        compare = (x < 5);
+        System.out.println(compare);
+
+        x = 4;
+        compare = (x < 5);
+        System.out.println(compare);
+
+        x = 5;
+        compare = (x < 5);
+        System.out.println(compare);
+
+        x = 6;
+        compare = (x < 5);
+        System.out.println(compare);
+
+        x = 10;
+        compare = (x < 5);
+        System.out.println(compare);
+    }
+}
