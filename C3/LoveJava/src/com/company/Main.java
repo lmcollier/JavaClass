1afa51a24165a5e462f2530e931b83f973e5844b
lmcollier <lmariecollier@gmail.com>
2019-05-15 12:37:38 -0500

    Chap3 Love Java

---
+++ C3/LoveJava/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("I love Java");
+
+        String i = "I";
+        String heart = " love ";
+        String code = "Java";
+        System.out.println(i + heart + code);
+    }
+}
