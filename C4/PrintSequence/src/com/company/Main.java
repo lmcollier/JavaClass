83b1b9ecaeda0c03726da18e59445ad60f07be7d
lmcollier <lmariecollier@gmail.com>
2019-05-17 12:53:04 -0500

    Print Sequence

---
+++ C4/PrintSequence/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int number = 108;
+
+        while (number > -100)
+        {
+            System.out.println(number -= 8);
+        }
+    }
+}
