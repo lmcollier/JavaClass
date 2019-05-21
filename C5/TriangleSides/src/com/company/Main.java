0afad3e4a6d08b1a54c70dc76d5214d5af779311
lmcollier <lmariecollier@gmail.com>
2019-05-21 16:40:20 -0500

    Triangle Sides

---
+++ C5/TriangleSides/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int triangleSideCount = getTriangleSideCount();
+        System.out.println("The number of sides a triangle has is: " + triangleSideCount);
+    }
+
+    private static int getTriangleSideCount()
+    {
+        return 3;
+    }
+}
