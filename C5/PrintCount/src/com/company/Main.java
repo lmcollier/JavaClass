669a10075942d7fd1414c9f8d3ae504d0cc6d084
lmcollier <lmariecollier@gmail.com>
2019-05-21 10:55:44 -0500

    Print Count

---
+++ C5/PrintCount/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        printCount(5);
+        printCount(20);
+    }
+
+    private static void printCount(int count)
+    {
+        System.out.println("The print count is: " + count);
+    }
+}
