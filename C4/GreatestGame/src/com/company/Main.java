5f013c55f32f19acca2ea9cd0dbc0a179b07a631
lmcollier <lmariecollier@gmail.com>
2019-05-21 16:23:20 -0500

    My Name

--- C4/GreatestGame/src/com/company/Main.java
+++ C4/GreatestGame/src/com/company/Main.java
@@ -5,11 +5,13 @@ public class Main
 
     public static void main(String[] args)
     {
-        // Bronze Exercise
 
         java.util.Scanner userInput = new java.util.Scanner(System.in);
         System.out.println("What is your name?");
         String name = userInput.next();
+
+        System.out.println("Would you like to play a guessing game? 1 = Yes, 2 = No");
+        int yes = userInput.nextInt();
 
         java.util.Random random = new java.util.Random();
         int number = random.nextInt(100);
@@ -19,65 +21,28 @@ public class Main
 
         int sum = 1;
 
-        while (number != guess)
+        if (yes == 1)
         {
-            while (number < guess)
+            while (number != guess)
             {
-                System.out.println("Your guess was too high. Please pick another number from 1 to 100: ");
-                guess = userInput.nextInt();
-                sum++;
+                while (number < guess)
+                {
+                    System.out.println("Your guess was too high. Please pick another number from 1 to 100: ");
+                    guess = userInput.nextInt();
+                    sum++;
+                }
+                while (number > guess)
+                {
+                    System.out.println("Your guess was too low. Please pick another number from 1 to 100: ");
+                    guess = userInput.nextInt();
+                    sum++;
+                }
             }
-            while (number > guess)
-            {
-                System.out.println("Your guess was too low. Please pick another number from 1 to 100: ");
-                guess = userInput.nextInt();
-                sum++;
-            }
+
+            System.out.println("Congratulations " + name + ", you have guessed the correct number! It took you " + sum +
+                    " to guess the correct number!");
         }
-        System.out.println("Congratulations " + name + ", you have guessed the correct number! It took you " + sum +
-        " to guess the correct number!");
 
-        // Silver Exercise
-
-        int sum1 = 1;
-        int sum2 = 1;
-        int games1 = 0;
-        int games2 = 0;
-        int totalGames = games1 + games2;
-        int average1 = sum1/games1;
-        int average2 = (sum1 + sum2)/games2;
-
-        java.util.Scanner userInput = new java.util.Scanner(System.in);
-        System.out.println("What is your name?");
-        String name = userInput.next();
-
-        java.util.Random random = new java.util.Random();
-        int number = random.nextInt(100);
-
-        System.out.println("Please guess any number from 1 to 100: ");
-        int guess = userInput.nextInt();
-
-        int sum = 1;
-
-        while (number != guess)
-        {
-            while (number < guess)
-            {
-                System.out.println("Your guess was too high. Please pick another number from 1 to 100: ");
-                guess = userInput.nextInt();
-                sum++;
-            }
-            while (number > guess)
-            {
-                System.out.println("Your guess was too low. Please pick another number from 1 to 100: ");
-                guess = userInput.nextInt();
-                sum++;
-            }
-        }
-            System.out.println("Congratulations " + name + ", you have guessed the correct number! It took you " + sum2 +
-                    " to guess the correct number! You have played " + totalGames + " and your average number of guesses " +
-                    "is " + average2 + ".");
-        }
-        // Gold Exercise
+        System.out.println("Would you like to play again? 1 = Yes, 2 = No");
     }
 }
---
+++ C5/ValueMethods/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String returnValue = getMyName();
+        System.out.println(returnValue);
+    }
+
+    public static String getMyName()
+    {
+        return "Cindy";
+    }
+}
