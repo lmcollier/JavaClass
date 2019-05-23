0544064631d014cd3f2c95b6d11347d34585f496
lmcollier <lmariecollier@gmail.com>
2019-05-23 11:00:36 -0500

    Switch Example Part Deux

---
+++ C5/SwitchExampleDeux/src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        java.util.Scanner userInput = new java.util.Scanner(System.in);
+        System.out.println("Please enter a number from 1-7: ");
+        int dayOfWeek = userInput.nextInt();
+
+        switch (dayOfWeek)
+        {
+            case 1:
+                System.out.println("Sunday");
+                break;
+
+            case 2:
+                System.out.println("Monday");
+                break;
+
+            case 3:
+                System.out.println("Tuesday");
+                break;
+
+            case 4:
+                System.out.println("Wednesday");
+                break;
+
+            case 5:
+                System.out.println("Thursday");
+                break;
+
+            case 6:
+                System.out.println("Friday");
+                break;
+
+            case 7:
+                System.out.println("Saturday");
+                break;
+
+            default:
+                System.out.println("Invalid value: " + dayOfWeek);
+
+        }
+    }
+}
