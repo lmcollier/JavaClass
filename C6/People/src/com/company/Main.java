e8d594ac04193f81c94930eaa1422b2f26d9a9a8
lmcollier <lmariecollier@gmail.com>
2019-05-29 08:53:23 -0500

    Person

---
+++ C6/People/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Person person = new Person("Larissa", "Collier");
+        System.out.println(person.getFullName());
+
+
+    }
+}
---
+++ C6/People/src/com/company/Person.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Person
+{
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFullName()
+    {
+        return lastName + ", " + firstName;
+    }
+}
